215fa275389e26d3ca966c24235ba351491adc56
jiyuujin <jiyuujinunite@gmail.com>
2022-10-26 21:29:56 +0900

    include 2022 implications

--- apps/2023-app/src/utils/constants.ts
+++ apps/2023-app/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const conferenceName = 'VS Code Conference JP'
-export const year = '2023'
+export const year = '2022 - 2023'
 export const conferenceNameWithYear = `${conferenceName} ${year}`
 export const organizationName = 'VS Code Meetup'
 export const description = `${organizationName} 主催の年次カンファレンス ${conferenceName} ${year} を今年も開催します！！！`
