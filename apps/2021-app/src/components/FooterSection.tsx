5f88e5c03de205d818a4459481cfdb5960e5c4bd
jiyuujin <jiyuujinunite@gmail.com>
2022-10-30 08:26:38 +0900

    switch link

--- apps/2021-app/src/components/FooterSection.tsx
+++ apps/2021-app/src/components/FooterSection.tsx
@@ -9,26 +9,34 @@ const FooterSection: FC = () => {
   return (
     <footer className={styles.footer}>
       <p className={styles.contact}>
-        <Link href={`/${lang}/code-for-conduct`}>
-          <a aria-label="Link to Code for Conduct" aria-describedby={i18next.t('code_of_conduct')}>
-            {i18next.t('code_of_conduct')}
-          </a>
-        </Link>
-        <Link href={`/${lang}/privacy-policy`}>
-          <a aria-label="Link to Privacy Policy" aria-describedby={i18next.t('privacy_policy')}>
-            {i18next.t('privacy_policy')}
-          </a>
-        </Link>
-        <Link href={`/${lang}/contact`}>
-          <a aria-label="Link to Contact" aria-describedby={i18next.t('contact')}>
-            {i18next.t('contact')}
-          </a>
-        </Link>
-        <Link href={`/${lang}/pre-event`}>
-          <a aria-label="Link to Pre Event" aria-describedby={i18next.t('pre_event')}>
-            {i18next.t('pre_event')}
-          </a>
-        </Link>
+        <a
+          href={`/${lang}/code-for-conduct`}
+          aria-label="Link to Code for Conduct"
+          aria-describedby={i18next.t('code_of_conduct')}
+        >
+          {i18next.t('code_of_conduct')}
+        </a>
+        <a
+          href={`/${lang}/privacy-policy`}
+          aria-label="Link to Privacy Policy"
+          aria-describedby={i18next.t('privacy_policy')}
+        >
+          {i18next.t('privacy_policy')}
+        </a>
+        <a
+          href={`/${lang}/contact`}
+          aria-label="Link to Contact"
+          aria-describedby={i18next.t('contact')}
+        >
+          {i18next.t('contact')}
+        </a>
+        <a
+          href={`/${lang}/pre-event`}
+          aria-label="Link to Pre Event"
+          aria-describedby={i18next.t('pre_event')}
+        >
+          {i18next.t('pre_event')}
+        </a>
       </p>
       <p className={styles.copyright}>
         {`Created © 2021 ${organizationName}. All Rights Reserved.`}
