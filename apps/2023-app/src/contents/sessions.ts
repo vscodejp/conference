8deaa7d869692a125703e0a1843a3ef109d46b5b
jiyuujin <jiyuujinunite@gmail.com>
2022-12-11 19:05:37 +0900

    timetable - update styles

--- apps/2023-app/src/contents/sessions.ts
+++ apps/2023-app/src/contents/sessions.ts
@@ -1,8 +1,8 @@
 import { ISession, TrackID, TrackName } from '@conference/shared/types'
 
-export const tracks: TrackID[] = ['track1', 'track2']
+export const tracks: TrackID[] = ['track1']
 
-export const trackNames: TrackName[] = ['Track A', 'Track B']
+export const trackNames: TrackName[] = ['Track A']
 
 // WIP
 export const sessions: ISession[] = [
